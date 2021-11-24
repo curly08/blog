4f02bcf5caa1824850a4029f64db1cf9e899f102
curly08 <mattynoakes08@gmail.com>
2021-11-24 18:43:27 -0500

    Add basic authentication for CUD actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: 'matt', password: 'kitkat', except: %i[index show]
+
   def index
     @articles = Article.all
   end
